e60202c46d44aa5a9b3735a053c0570b61538168
Danni <38550500+isdanni@users.noreply.github.com>
2020-08-02 11:04:35 +0800

    Create usr_test.clj

---
+++ test/ratings/handler/usr_test.clj
@@ -0,0 +1,11 @@
+(ns ratings.handler.usr-test
+  (:require [clojure.test :refer :all]
+            [integrant.core :as ig]
+            [ring.mock.request :as mock]
+            [ratings.handler.example :as example]))
+
+(deftest smoke-test
+  (testing "usr exists"
+    (let [handler  (ig/init-key :ratings.handler/usr {})
+          response (handler (mock/request :get "/usr"))]
+      (is (= :ataraxy.response/ok (first response)) "response ok"))))
